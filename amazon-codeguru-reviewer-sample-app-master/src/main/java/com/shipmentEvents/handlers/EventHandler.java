e82c7dc86449c91772613225cb27627c527291ba
gauravbj <gauravbjain.25@gmail.com>
2024-08-15 16:16:17 -0700

    Update EventHandler.java

--- amazon-codeguru-reviewer-sample-app-master/src/main/java/com/shipmentEvents/handlers/EventHandler.java
+++ amazon-codeguru-reviewer-sample-app-master/src/main/java/com/shipmentEvents/handlers/EventHandler.java
@@ -34,6 +34,8 @@ import org.apache.commons.lang3.tuple.Pair;
 
 public class EventHandler implements RequestHandler<ScheduledEvent, String> {
 
+    
+
     /**
      * Shipment events for a carrier are uploaded to separate S3 buckets based on the source of events. E.g., events originating from
      * the hand-held scanner are stored in a separate bucket than the ones from mobile App. The Lambda processes events from multiple
