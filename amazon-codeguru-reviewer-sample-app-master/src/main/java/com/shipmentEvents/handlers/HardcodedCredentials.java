ae9ebb62b826240dc12ae5e6e6e0dd0a0ca302cc
Gaurav Jain <gaurjaig@amazon.com>
2024-08-25 16:48:59 -0700

    Apply patch from file

--- amazon-codeguru-reviewer-sample-app-master/src/main/java/com/shipmentEvents/handlers/HardcodedCredentials.java
+++ amazon-codeguru-reviewer-sample-app-master/src/main/java/com/shipmentEvents/handlers/HardcodedCredentials.java
@@ -14,7 +14,7 @@ public class HardcodedCredentials {
     public void createSqlConnectionNoncompliant() throws Exception {
         // Noncompliant: password is hardcoded.
         final Connection connection = DriverManager.getConnection("some url",
-                "username", "password");
+                "username", System.getenv("PASSWORD_VALUE"));
         connection.close();
     }
     // {/fact}
